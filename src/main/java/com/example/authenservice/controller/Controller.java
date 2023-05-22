f18846101d90c3f0e566dd0a3b73af9860ae056a
Nguyen Tien Dai <daint@vnpay.vn>
2023-05-22 17:15:11 +0700

    .

--- src/main/java/com/example/authenservice/controller/Controller.java
+++ src/main/java/com/example/authenservice/controller/Controller.java
@@ -2,18 +2,15 @@ package com.example.authenservice.controller;
 
 import com.example.authenservice.request.LoginRequest;
 import com.example.authenservice.service.AuthenService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/v1/authen")
-@Slf4j
 public class Controller {
     private static final long start = System.currentTimeMillis();
     private static final  long end = start + 10000;
@@ -25,6 +22,7 @@ public class Controller {
 //        return ResponseEntity.ok(authenService.login(loginRequest));
 //    }
     @PostMapping("/login")
+<<<<<<< HEAD
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) throws InterruptedException {
         log.info("into instance");
         log.info("end date is: {}", end);
@@ -34,5 +32,9 @@ public class Controller {
             return ResponseEntity.ok("data from instance error");
         }
         return ResponseEntity.internalServerError().build();
+=======
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
+        return ResponseEntity.ok(authenService.login(loginRequest));
+>>>>>>> parent of 8823946 (update)
     }
 }
