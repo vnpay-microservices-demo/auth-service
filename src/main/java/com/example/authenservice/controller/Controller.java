81a680449540001beef42386821dc5cc57ebbd32
Nguyen Tien Dai <daint@vnpay.vn>
2023-05-22 17:20:16 +0700

    .

--- src/main/java/com/example/authenservice/controller/Controller.java
+++ src/main/java/com/example/authenservice/controller/Controller.java
@@ -2,7 +2,10 @@ package com.example.authenservice.controller;
 
 import com.example.authenservice.request.LoginRequest;
 import com.example.authenservice.service.AuthenService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,30 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/authen")
+@Slf4j
 public class Controller {
     private static final long start = System.currentTimeMillis();
-    private static final  long end = start + 10000;
+    private static final long end = start + 10000;
     @Autowired
     private AuthenService authenService;
-//    @PostMapping("/login")
+
+    //    @PostMapping("/login")
 //    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) throws InterruptedException {
 //        log.info("into instance");
 //        return ResponseEntity.ok(authenService.login(loginRequest));
 //    }
     @PostMapping("/login")
-<<<<<<< HEAD
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) throws InterruptedException {
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         log.info("into instance");
         log.info("end date is: {}", end);
         long currenDate = System.currentTimeMillis();
         log.info("current date is: {}", currenDate);
-        if(currenDate < end){
+        if (currenDate < end) {
             return ResponseEntity.ok("data from instance error");
         }
         return ResponseEntity.internalServerError().build();
-=======
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
-        return ResponseEntity.ok(authenService.login(loginRequest));
->>>>>>> parent of 8823946 (update)
     }
 }
